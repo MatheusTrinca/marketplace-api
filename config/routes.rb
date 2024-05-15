fc81c58fb55d2051adcdd3dc67943306ed0907f6
matheustrinca <matheusfelipetrinca@gmail.com>
2024-05-15 12:21:00 -0300

    chore: add initial config and docker

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# LIVE 4 22:40
+# https://www.youtube.com/watch?v=r_zb4aCpWu8&list=PLkjb8D53F2ilcHtYTekiLCt-RDNUSg6XV&index=5&ab_channel=skilldevs
